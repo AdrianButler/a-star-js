cd4ed4ed873ce9217e966f85e91afb41f43ba56f
Adrian Butler Jr <butlerjr123@gmail.com>
2022-09-17 18:03:09 -0500

    created customizable grid that will be used to represent nodes

--- app.js
+++ app.js
@@ -1,18 +1,61 @@
 import Node from "./modules/node.js";
 
-let rows = 5;
-let cols = 5;
+const grid = document.getElementById("grid");
 
-let width = 100;
-let height = 100;
+let rows = 10;
+let cols = 10;
+
+let width = 50;
+let height = 50;
 
 let nodes = [];
 
 for (let i = 0; i < rows; i++)
 {
+	if (!nodes[i])
+	{
+		nodes[i] = [];
+	}
 	for (let j = 0; j < cols; j++)
 	{
 		nodes[i][j] = new Node();
 	}
 }
 
+
+for (let i = 0; i < nodes.length; i++)
+{
+	for (let j = 0; j < nodes[i].length; j++)
+	{
+		const square = document.createElement("div");
+
+		switch (nodes[i][j].status)
+		{
+			case "filled":
+				square.backgroundColor = "blue";
+				break;
+			case "obstacle":
+				square.backgroundColor = "black";
+				break;
+			case "goal":
+				square.backgroundColor = "gold";
+				break;
+		}
+		square.style.position = "absolute";
+		square.style.width = `${width - 1}px`;
+		square.style.height = `${height - 1}px`;
+		square.style.left = `${i * width}px`;
+		square.style.top = `${j * height}px`;
+		square.style.border = "1px solid black"
+
+		//todo add event listener
+
+		nodes[i][j].htmlElement = square;
+
+		console.log("here");
+		grid.appendChild(square);
+	}
+}
+
+grid.style.width = `${width * rows}px`;
+grid.style.height = `${height * cols}px`;
--- modules/node.js
+++ modules/node.js
@@ -1,10 +1,16 @@
 export default class Node
 {
+
 	constructor()
 	{
 		this.open = false;
-		this.blocked = false;
-		this.fCost = 0;
+		this.fCost = 0; // Total Cost | g + h
+		this.parent = undefined;
+		this.gCost = 0; // Distance from starting node
+		this.hCost = 0; // Distance from end node
+		this.status = "empty"; // empty, filled, obstacle, goal
+		this.htmlElement = undefined;
 	}
+
 }
 
