488fc6c789f317d452995feb8f31c6bd0da490da
Adrian Butler Jr <butlerjr123@gmail.com>
2022-09-17 22:03:00 -0500

    added ability to draw obstacles

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ import Node from "./modules/node.js";
 
 const grid = document.getElementById("grid");
 
-let rows = 10;
-let cols = 10;
+let rows = 20;
+let cols = 20;
 
 let width = 50;
 let height = 50;
 
 let nodes = [];
 
+let currentSelectedType;
 setUpGrid();
 
 const start = () =>
@@ -19,6 +20,7 @@ const start = () =>
 
 function setUpGrid()
 {
+
 	for (let i = 0; i < rows; i++)
 	{
 		if (!nodes[i])
@@ -29,31 +31,25 @@ function setUpGrid()
 		{
 			nodes[i][j] = new Node();
 			const square = document.createElement("div");
-			square.style.position = "absolute";
-			square.style.width = `${width - 1}px`;
-			square.style.height = `${height - 1}px`;
-			square.style.left = `${i * width}px`;
-			square.style.top = `${j * height}px`;
+			square.style.width = `${width}px`;
+			square.style.height = `${height}px`;
 			square.style.border = "1px solid black";
 
 			nodes[i][j].htmlElement = square;
 
-			square.onclick = () =>
+			square.onmousedown = () =>
 			{
-				const status = nodes[i][j].status;
-				const nodeElement = nodes[i][j].htmlElement;
-				if (status === "empty")
-				{
-					nodes[i][j].status = "obstacle";
-					nodeElement.style.backgroundColor = "black";
-				}
-				else if (status === "obstacle")
-				{
-					nodes[i][j].status = "empty"
-					nodeElement.style.backgroundColor = "white";
-				}
+				currentSelectedType = nodes[i][j].status;
+				nodes[i][j].flip();
 			};
 
+			square.onmouseover = (event) =>
+			{
+				if (event.buttons === 1 && nodes[i][j].status === currentSelectedType)
+				{
+					nodes[i][j].flip();
+				}
+			};
 
 
 			grid.appendChild(square);
@@ -64,8 +60,8 @@ function setUpGrid()
 
 function setGridSize()
 {
-	grid.style.width = `${width * rows}px`;
-	grid.style.height = `${height * cols}px`;
+	grid.style.width = `${width * rows + rows + 1}px`;
+	grid.style.height = `${height * cols + cols + 1}px`;
 }
 
 console.log(nodes);
